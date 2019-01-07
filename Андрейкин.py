cffb274359de242929420e93a676d9f923897956
Andryss <andr.krivosheev@gmail.com>
2019-01-07 14:36:56 +0300

    Сделал ограничение шаров и улучшил физику платформы

--- Андрейкин.py
+++ Андрейкин.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 255))
 clock = pygame.time.Clock()
 balls = {}
-door_size = (80, 15)
+door_size = (100, 40)
 door = [size[0] // 2 - 40, size[1] - 5 - door_size[1]]
 moves = [(-1, -1), (1, -1), (1, 1), (-1, 1)]
 move = 10
@@ -17,7 +17,7 @@ pygame.time.set_timer(k, 10)
 new = 0
 
 running = True
-pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], 80, 15))
+pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], door_size[0], door_size[1]))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -26,9 +26,10 @@ while running:
             new += 10
         if new == 5000:
             new = 0
-            pos = (random.randint(10, size[0] - 10), 15)
-            pygame.draw.circle(screen, (255, 255, 255), pos, 10)
-            balls[pos] = random.choice(moves)
+            if len(balls) <= 4:
+                pos = (random.randint(10, size[0] - 10), 15)
+                pygame.draw.circle(screen, (255, 255, 255), pos, 10)
+                balls[pos] = random.choice(moves)
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
             if door[0] < size[0] - 85:
                 door[0] += 5
@@ -95,6 +96,13 @@ while running:
                             balls[ball] = moves[1]
                         elif balls[ball] == moves[3]:
                             balls[ball] = moves[0]
+                if (ball[1] < door[1] + door_size[1]) and (ball[1] > door[1] - 10):
+                    if ball[0] == door[0] - 10:
+                        if balls[ball] == moves[2]:
+                            balls[ball] = moves[0]
+                    elif ball[0] == door[0] + 10 + door_size[0]:
+                        if balls[ball] == moves[3]:
+                            balls[ball] = moves[1]
                 
     pygame.display.flip()            
     clock.tick(140)       
