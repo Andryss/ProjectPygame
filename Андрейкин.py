0eda6af8c8f0aad9a421bb5c69cd613bc597d0dc
Andryss <andr.krivosheev@gmail.com>
2019-01-07 14:21:20 +0300

    Сделал, чтобы от платформы отскакивали шарики

--- Андрейкин.py
+++ Андрейкин.py
@@ -7,22 +7,24 @@ screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 255))
 clock = pygame.time.Clock()
 balls = {}
-door = [size[0] // 2 - 40, size[1] - 15]
+door_size = (80, 15)
+door = [size[0] // 2 - 40, size[1] - 5 - door_size[1]]
 moves = [(-1, -1), (1, -1), (1, 1), (-1, 1)]
 move = 10
-pygame.time.set_timer(move, 50000)
+pygame.time.set_timer(move, 500)
 k = 10
 pygame.time.set_timer(k, 10)
 new = 0
 
 running = True
+pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], 80, 15))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if k:
             new += 10
-        if new == 4000:
+        if new == 5000:
             new = 0
             pos = (random.randint(10, size[0] - 10), 15)
             pygame.draw.circle(screen, (255, 255, 255), pos, 10)
@@ -30,14 +32,18 @@ while running:
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
             if door[0] < size[0] - 85:
                 door[0] += 5
+                screen.fill((0, 0, 255))
+                pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], door_size[0], door_size[1]))
         if pygame.key.get_pressed()[pygame.K_LEFT]:
             if door[0] > 5:
                 door[0] -= 5
+                screen.fill((0, 0, 255))
+                pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], door_size[0], door_size[1]))
         
             
         if balls != {} and move:
             screen.fill((0, 0, 255))
-            pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], 80, 15))
+            pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], door_size[0], door_size[1]))
             for ball in balls.keys():
                 if balls[ball] == moves[0]:
                     ball_cop = (ball[0] - 1, ball[1] - 1)
@@ -83,6 +89,12 @@ while running:
                         balls[ball] = moves[1]
                     elif balls[ball] == moves[3]:
                         balls[ball] = moves[0]
+                if (ball[0] > door[0]) and (ball[0] < door[0] + door_size[0]):
+                    if ball[1] == door[1] - 10:
+                        if balls[ball] == moves[2]:
+                            balls[ball] = moves[1]
+                        elif balls[ball] == moves[3]:
+                            balls[ball] = moves[0]
                 
     pygame.display.flip()            
     clock.tick(140)       
