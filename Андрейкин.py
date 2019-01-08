45828f423a7478e8d1dab2f8078b868d949dae11
Andryss <andr.krivosheev@gmail.com>
2019-01-08 12:00:31 +0300

    Добавил очки и регулировку уровня

--- Андрейкин.py
+++ Андрейкин.py
@@ -15,6 +15,9 @@ pygame.time.set_timer(move, 500)
 k = 10
 pygame.time.set_timer(k, 10)
 new = 0
+points = 0
+level = 5
+kolvo = 0
 
 running = True
 pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], door_size[0], door_size[1]))
@@ -26,12 +29,13 @@ while running:
             new += 10
         if new == 5000:
             new = 0
-            if len(balls) <= 4:
+            if len(balls) < level:
+                kolvo += 1
                 pos = (random.randint(10, size[0] - 10), 15)
                 pygame.draw.circle(screen, (255, 255, 255), pos, 10)
                 balls[pos] = random.choice(moves)
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            if door[0] < size[0] - 85:
+            if door[0] < size[0] - 5 - door_size[0]:
                 door[0] += 5
                 screen.fill((0, 0, 255))
                 pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], door_size[0], door_size[1]))
@@ -40,11 +44,23 @@ while running:
                 door[0] -= 5
                 screen.fill((0, 0, 255))
                 pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], door_size[0], door_size[1]))
+                
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                level += 1
+            if event.key == pygame.K_DOWN:
+                if level > 1:
+                    level -= 1
+                    kolvo -= 1
+                    balls.popitem()
         
             
         if balls != {} and move:
+            points += 1 * kolvo
             screen.fill((0, 0, 255))
             pygame.draw.rect(screen, (255, 255, 255), (door[0], door[1], door_size[0], door_size[1]))
+            screen.blit(pygame.font.Font(None, 50).render(str(points), 1, (100, 255, 100)), (5, 5))
+            screen.blit(pygame.font.Font(None, 50).render("LEVEL {}".format(kolvo) , 1, (100, 255, 100)), (5, 45))
             for ball in balls.keys():
                 if balls[ball] == moves[0]:
                     ball_cop = (ball[0] - 1, ball[1] - 1)
@@ -96,15 +112,14 @@ while running:
                             balls[ball] = moves[1]
                         elif balls[ball] == moves[3]:
                             balls[ball] = moves[0]
-                if (ball[1] < door[1] + door_size[1]) and (ball[1] > door[1] - 10):
-                    if ball[0] == door[0] - 10:
+                elif (ball[1] <= door[1] + door_size[1]) and (ball[1] >= door[1] - 10):
+                    if (ball[0] >= door[0] - 10) and (ball[0] <= door[0] + 10 + door_size[0]):
                         if balls[ball] == moves[2]:
                             balls[ball] = moves[0]
-                    elif ball[0] == door[0] + 10 + door_size[0]:
-                        if balls[ball] == moves[3]:
+                        elif balls[ball] == moves[3]:
                             balls[ball] = moves[1]
                 
     pygame.display.flip()            
-    clock.tick(140)       
+    clock.tick(140)      
 
 pygame.quit()
